3492c0d93288fae8af1e1e3b4f6350e797767981
Nathaniel Li <nthnll314@gmail.com>
2018-05-20 14:34:25 -0700

    Server MetaVis integration

--- HeatmapMetaVis.py
+++ HeatmapMetaVis.py
@@ -4,71 +4,128 @@ import pandas as pd
 import sys
 import os.path as op
 from bokeh.io import show, output_file
+from bokeh.embed import file_html
 from metaVis import *
 
 
-if len(sys.argv) > 1:
-    homeParam = sys.argv[1]
-else:
-    homeParam = 'mzWork'
+def error_check(data, ptid_md, measures_md):
+    data_colnames = list(data.columns.values)
+    data_rownames = list(data.index)
+    ptid_names = list(ptid_md.index)
+    measures_names = list(measures_md.index)
 
-homeFolders = dict(mzWork='C:/Users/mihuz/Documents',
-                   afgWork='A:/gitrepo')
-home = homeFolders[homeParam]
+    if (data.shape[1] != measures_md.shape[0]):
+        error = "<p>Error: Number of measurements in base dataset does not match the number of measurements in the measurement metadata.</br>"
+        error += "&emsp;Base Data: " + str(data.shape[1]) + "</br>"
+        error += "&emsp;Measures Metadata: " + str(measures_md.shape[0])
+        return error
 
-# Importing files as dataframes
+    if (data.shape[0] != ptid_md.shape[0]):
+        error = "<p>Error: Number of PtID's in base dataset does not match the number of PtID's in the PtID metadata. </br>"
+        error += "&emsp;Base Data: " + str(data.shape[0]) + "</br>"
+        error += "&emsp;PtID's Metadata: " + str(ptid_md.shape[0])
+        error += "</p>"
+        return error
 
-data = pd.read_csv(op.join(home, 'metadataVis', 'data', 'IR_levels097.csv'), index_col=0)
-measures_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'metacols097.csv'), index_col=0)
-ptid_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'metarows097.csv'), index_col=0)
-# data = pd.read_csv(op.join(home, 'metadataVis', 'data', 'wideform_test.csv'), index_col=0)
-# measures_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'wideform_measuremd_test.csv'), index_col=0)
-# ptid_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'wideform_ptidmd_test.csv'), index_col=0)
+    if (ptid_names != data_rownames):
+        error = "<p>Error: PtID's in base dataset do not match PtID's in PtID metadata.</p>"
+        return error
 
-# ERROR CHECKING
-data_colnames = list(data.columns.values)
-data_rownames = list(data.index)
-ptid_names = list(ptid_md.index)
-measures_names = list(measures_md.index)
+    if (measures_names != data_colnames):
+        error = "<p>Error: Measures in base dataset do not match measures in measurement metadata.</p>"
+        return error
+    return None
 
-if (data.shape[1] != measures_md.shape[0]):
-    print("Error: Number of measurements in base dataset does not match the number of measurements in the measurement metadata.")
-    print("       Base Data: ", data.shape[1])
-    print("       Measures Metadata: ", measures_md.shape[0])
-    sys.exit()
-if (data.shape[0] != ptid_md.shape[0]):
-    print("Error: Number of PtID's in base dataset does not match the number of PtID's in the PtID metadata.")
-    print("       Base Data: ", data.shape[0])
-    print("       PtID's Metadata: ", ptid_md.shape[0])
-    sys.exit()
+def gen_heatmap_html(data=None, row_md=None, col_md=None,
+                     metric=None, method=None,
+                     standardize=True, impute=True, static=False):
+    # TODO - Make sure data is submitted in html
+    ret_val = {}
+    ret_val['error'] = error_check(data, row_md, col_md)
+    if ret_val['error'] is not None:
+        return ret_val
 
-if ptid_names != data_rownames:
-    print("Error: PtID's in base dataset do not match PtID's in PtID metadata.")
-    sys.exit()
+    ptid_md = row_md
+    measures_md = col_md
+    # TODO - double check clusterData param handling
+    data, ptid_md, measures_md, rowDend, colDend = clusterData(data, ptid_md, measures_md,
+                                         metric=metric,
+                                         method=method,
+                                         standardize=standardize,
+                                         impute=impute)
+    sources = initSources(data, ptid_md, measures_md)
 
-if measures_names != data_colnames:
-    print("Error: Measures in base dataset do not match measures in measurement metadata.")
-    sys.exit()
+    cbDict = initCallbacks(sources)
 
-# data, measures_md = filterData(data, measures_md, method='mean', params={'thresh':0.0001})
-# data, measures_md = filterData(data, measures_md, method='meanTopN', params={'ncols':50})
+    layout = generateLayout(sources, cbDict, rowDend, colDend)
+    # TODO - Return html, or generate file?
+    # output_file('ServerMetaVis.html', title='ServerMetaVis')
+    ret_val['heatmap_html'] = file_html(layout, resources=CDN)
+    return ret_val
 
-# data, ptid_md = filterData(data, ptid_md, method='pass')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        homeParam = sys.argv[1]
+    else:
+        homeParam = 'mzWork'
+
+    homeFolders = dict(mzWork='C:/Users/mihuz/Documents',
+                       afgWork='A:/gitrepo')
+    home = homeFolders[homeParam]
+
+    # Importing files as dataframes
+
+    data = pd.read_csv(op.join(home, 'metadataVis', 'data', 'IR_levels097.csv'), index_col=0)
+    measures_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'metacols097.csv'), index_col=0)
+    ptid_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'metarows097.csv'), index_col=0)
+    # data = pd.read_csv(op.join(home, 'metadataVis', 'data', 'wideform_test.csv'), index_col=0)
+    # measures_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'wideform_measuremd_test.csv'), index_col=0)
+    # ptid_md = pd.read_csv(op.join(home, 'metadataVis', 'data', 'wideform_ptidmd_test.csv'), index_col=0)
+
+    # ERROR CHECKING
+    data_colnames = list(data.columns.values)
+    data_rownames = list(data.index)
+    ptid_names = list(ptid_md.index)
+    measures_names = list(measures_md.index)
+
+    if (data.shape[1] != measures_md.shape[0]):
+        print("Error: Number of measurements in base dataset does not match the number of measurements in the measurement metadata.")
+        print("       Base Data: ", data.shape[1])
+        print("       Measures Metadata: ", measures_md.shape[0])
+        sys.exit()
+    if (data.shape[0] != ptid_md.shape[0]):
+        print("Error: Number of PtID's in base dataset does not match the number of PtID's in the PtID metadata.")
+        print("       Base Data: ", data.shape[0])
+        print("       PtID's Metadata: ", ptid_md.shape[0])
+        sys.exit()
+
+    if ptid_names != data_rownames:
+        print("Error: PtID's in base dataset do not match PtID's in PtID metadata.")
+        sys.exit()
+
+    if measures_names != data_colnames:
+        print("Error: Measures in base dataset do not match measures in measurement metadata.")
+        sys.exit()
+
+    # data, measures_md = filterData(data, measures_md, method='mean', params={'thresh':0.0001})
+    # data, measures_md = filterData(data, measures_md, method='meanTopN', params={'ncols':50})
+
+    # data, ptid_md = filterData(data, ptid_md, method='pass')
 
 
-data, ptid_md, measures_md, rowDend, colDend = clusterData(data, ptid_md, measures_md,
-                                     metric='euclidean',
-                                     method='ward',
-                                     standardize=True,
-                                     impute=True)
+    data, ptid_md, measures_md, rowDend, colDend = clusterData(data, ptid_md, measures_md,
+                                         metric='euclidean',
+                                         method='ward',
+                                         standardize=True,
+                                         impute=True)
 
-# Creating overall data source
-sources = initSources(data, ptid_md, measures_md)
+    # Creating overall data source
+    sources = initSources(data, ptid_md, measures_md)
 
-cbDict = initCallbacks(sources)
+    cbDict = initCallbacks(sources)
 
-layout = generateLayout(sources, cbDict, rowDend, colDend)
+    layout = generateLayout(sources, cbDict, rowDend, colDend)
 
-output_file('HeatmapMetaVis.html', title='HeatmapMetaVis')
-show(layout)
-
+    output_file('HeatmapMetaVis.html', title='HeatmapMetaVis')
+    show(layout)
---
+++ mdvServer_exp.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+from twisted.web import server, resource, static
+from twisted.internet import reactor, endpoints, task, threads
+from twisted.internet.defer import Deferred
+import cgi
+import argparse
+from io import StringIO
+import pandas as pd
+from twisted.web.util import Redirect
+from twisted.web.util import redirectTo
+from twisted.python import log
+
+#from example_server_plot import exampleVis
+from HeatmapMetaVis import gen_heatmap_html
+
+# TODO - Popup if file upload is empty; popup for errors
+def _prepArgs(request):
+    kwargs = {}
+    # request.write("<html>" + str(request.args) + "<body>")
+    base_data = None
+    row_md = None
+    col_md = None
+    for k,v in request.args.iteritems():
+        if k.find('File') >= 0:
+            sio = StringIO(unicode(request.args[k][0]))
+            tmp = pd.read_csv(sio, index_col=0)
+            kwargs[k.replace('File', '')] = tmp
+            # if k == "col_mdFile":
+            #      request.write("<html> TEST TEST TEST \n" + str(kwargs["col_md"]) + "<body>")
+
+    # request.write("\n\n\n<html>" + str(kwargs) + "<body>")
+    if 'static' in request.args:
+        kwargs['static'] = True
+    if 'standardize' in request.args:
+        kwargs['standardize'] = True
+    kwargs['metric'] = request.args['metric'][0]
+    kwargs['method'] = request.args['method'][0]
+
+    key_string = [k for k in kwargs]
+    #request.write("<html>" + str(key_string) + "<body>")
+    ret_map = gen_heatmap_html(**kwargs)
+    if(ret_map['error'] is not None):
+        # TODO - More Elegant Error Display
+        request.write("<html>" + ret_map['error'] + "<body>")
+        return request
+    html = ret_map['heatmap_html']
+    # html = exampleVis(**kwargs)
+    # print(html[:100])
+    request.write(html.encode('utf-8'))
+    return request
+
+class FileUpload(resource.Resource):
+    isLeaf = True
+    def render_GET(self, request):
+        return redirectHome.render(request)
+
+    def _delayedPOSTRender(self, request):
+        #request.write("\n\n\nUpload complete.</body></html>")
+        request.finish()
+
+    def render_POST(self, request):
+        #request.write('<html><body>')
+        d = threads.deferToThread(_prepArgs, request)
+        d.addCallback(self._delayedPOSTRender)
+        d.addErrback(log.err)
+        return server.NOT_DONE_YET
+
+class RootResource(resource.Resource):
+    isLeaf = False
+    def getChild(self, name, request):
+        if name == '':
+            return self
+        return resource.Resource.getChild(self, name, request)
+    def render_GET(self, request):
+        return redirectHome.render(request)
+
+redirectHome = Redirect('home')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Start metadataVis web server.')
+    parser.add_argument('--port', metavar='PORT', type=int, default=5097)
+    args = parser.parse_args()
+
+    root = RootResource()
+    root.putChild('home', static.File('./homepage'))
+    root.putChild('viz', FileUpload())
+
+    site = server.Site(root)
+    reactor.listenTCP(args.port, site)
+    reactor.run()
