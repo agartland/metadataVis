39a4ae9a5fbfc50c7760b3c06834c25e5bb5e0bd
Nathaniel Li <nthnll314@gmail.com>
2018-07-01 19:54:32 -0700

    Organizing constants

--- MetaVisLauncherConfig.py
+++ MetaVisLauncherConfig.py
@@ -1,7 +1,12 @@
+# - Paths
 # Edit python path here
 python3_path = "C:\Anaconda3\python.exe"
-output_file = "Heatmap.html"
 launcher = "Py3MetaVisLauncher.py"
+
+# - File Handling
+output_file = "Heatmap.html"
 tmp_dir = "tmpdata"
+
+# - Error Logging
 error_dir = "MetaVisErrorFiles"
 error_file = "MetaVisError.txt"
